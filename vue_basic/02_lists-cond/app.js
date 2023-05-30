5bd3eff7e7faf078a7a4c15bbc51c085b8b3b4d7
Seogun95 <ksj083895@gmail.com>
2023-05-30 23:55:47 +0900

    feat: list-conde

--- vue_basic/02_lists-cond/app.js
+++ vue_basic/02_lists-cond/app.js
@@ -1,10 +1,48 @@
 const { createApp } = Vue;
+
+const test = [
+  {
+    name: '선중',
+    age: 29,
+  },
+  {
+    name: '미진',
+    age: 21,
+  },
+  {
+    name: '희진',
+    age: 31,
+  },
+  {
+    name: '시욱',
+    age: 29,
+  },
+  {
+    name: '은광',
+    age: 26,
+  },
+];
+
 const app = createApp({
   data() {
-    return { goals: [] };
+    return { goals: [], listValue: '', test };
   },
   watch: {},
   computed: {},
-  methods: {},
+  methods: {
+    addGoals() {
+      if (this.listValue) {
+        this.goals.push(this.listValue);
+        this.listValue = '';
+        this.$refs.goalRef.focus();
+      }
+    },
+    removeGoals(index) {
+      this.goals.splice(index, 1);
+    },
+  },
+  mounted() {
+    this.$refs.goalRef.focus();
+  },
 });
 app.mount('#user-goals');
